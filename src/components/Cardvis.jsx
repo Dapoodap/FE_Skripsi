3acf2d4db3e70c9db39d6bba29f31ed1a3f154e4
daffaRadhitya <111202012648@mhs.dinus.ac.id>
2023-11-19 10:09:56 +0700

    add card

---
+++ src/components/Cardvis.jsx
@@ -0,0 +1,52 @@
+import { Button, Card, Container } from "react-bootstrap";
+import gbr1 from '../assets/card1.png'
+import gbr2 from '../assets/card2.png'
+import gbr3 from '../assets/card3.png'
+
+function Cardvis() {
+  return (
+    <>
+      <Container style={{padding:'10px',display:'flex',justifyContent:'center',gap:'60px',flexWrap:'wrap'}}>
+        <Card style={{ width: "18rem",border:'2px solid black',position:'relative'}}>
+          <Card.Body style={{ height:'220px', padding:'15px 10px 15px 10px '}}>
+            <Card.Title>
+                <h2 style={{ fontSize:'27px',fontWeight:'600',lineHeight:'28px',letterSpacing:'1px' }}>Kenyamanan</h2>
+            </Card.Title>
+            <br />
+            <Card.Text>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum et accusamus minima necessitatibus, at magnam ea delectus expedita quae temporibus!</p>
+            </Card.Text>
+          </Card.Body>
+          <Card.Img variant="bottom" src={gbr2} style={{border:'2px solid black',bottom:'0'}} className="img-fluid" />
+        </Card>
+        <Card style={{ width: "18rem",border:'2px solid black',position:'relative'}}>
+          <Card.Body style={{ height:'220px', padding:'15px 10px 15px 10px '}}>
+            <Card.Title>
+                <h2 style={{ fontSize:'27px',fontWeight:'600',lineHeight:'28px',letterSpacing:'1px' }}>Keamanan</h2>
+            </Card.Title>
+            <br />
+            <Card.Text>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum et accusamus minima necessitatibus, at magnam ea delectus expedita quae temporibus!</p>
+            </Card.Text>
+          </Card.Body>
+          <Card.Img variant="bottom" src={gbr3} style={{border:'2px solid black',bottom:'0'}} className="img-fluid" />
+        </Card>
+        <Card style={{ width: "18rem",border:'2px solid black',position:'relative'}}>
+          <Card.Body style={{ height:'220px', padding:'15px 10px 15px 10px '}}>
+            <Card.Title>
+                <h2 style={{ fontSize:'27px',fontWeight:'600',lineHeight:'28px',letterSpacing:'1px' }}>Ketenangan</h2>
+            </Card.Title>
+            <br />
+            <Card.Text>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum et accusamus minima necessitatibus, at magnam ea delectus expedita quae temporibus!</p>
+            </Card.Text>
+          </Card.Body>
+          <Card.Img variant="bottom" src={gbr1} style={{border:'2px solid black',bottom:'0'}} className="img-fluid" />
+        </Card>
+        
+      </Container>
+    </>
+  );
+}
+
+export default Cardvis;
--- src/components/Homescreen.jsx
+++ src/components/Homescreen.jsx
@@ -1,27 +1,48 @@
-    import { Col, Container, Row } from 'react-bootstrap'
-    import Navigation from './Navigation'
+import { Col, Container, Image, Row, } from "react-bootstrap";
+import kos from "../assets/kos.jpg";
+import Navigation from "./Navigation";
+import Cardvis from "./Cardvis";
 
-    function Homescreen() {
-    return (
-        <>
-            <Navigation/>
-            <Container >
-                <Row > 
-                    <Col sm={12} md={6} style={{ border:'1px solid black',padding:'20px'}}>
-                        <h1 style={{fontFamily:'Sofia, cursive',fontSize:'50px',fontWeight:'500',color:'#7C6A46'}}>Paradise View</h1>
+function Homescreen() {
+  return (
+    <>
+      <Navigation />
+      <br />
+      <br />
+      <Container fluid>
+        <Row>
+          <Col xs={12} xl={6} md={12} >
+            <div className="centred" style={{ height: "100%", padding: "20px",display:'flex',flexDirection:'column',justifyContent:'center'}}>
+            <h1 style={{fontFamily:'Sofia, cursive',fontSize:'50px',fontWeight:'500',color:'#7C6A46'}}>Paradise View</h1>
                         <p style={{fontSize:'50px',fontWeight:'700'}}>
                         Hotel for every moment rich in emotion
                         </p>
-                        <p style={{marginTop:'-10px',fontSize:'15px',fontWeight:'500'}}>Every moment feels like the first time in paradise view</p>
-                        <a href="#" style={{ textDecoration:'none',color:'black',padding:'10px 30px 10px 30px',border:'1px solid black'}}>
+                        <p style={{fontSize:'15px',fontWeight:'500'}}>Every moment feels like the first time in paradise view</p>
+                        <a className="btnEx" href="#" style={{ maxWidth:'300px',textAlign:'center',textDecoration:'none',color:'white',padding:'10px 30px 10px 30px',border:'1px solid black',borderRadius:'30px',backgroundColor:'#7C6A46'}}>
                             Sewa Kamar
                         </a>
-                    </Col >
-                    <Col style={{ border:'1px solid black',padding:'20px'}}>2</Col >
-                </Row>
-            </Container>
-        </>
-    )
-    }
+            </div>
+          </Col>
+          <Col xs={12} xl={6} md={12}>
+            <Image
+              src={kos} rounded
+              alt="Placeholder Image"
+              className="img-fluid"
+              
+            />
+          </Col>
+        </Row>
+      </Container>
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1 style={{fontSize:'55px',fontWeight:'700',color:'#7C6A46'}} className="text-center">Our Mission</h1>
+      <h2 style={{fontSize:'40px',fontWeight:'400',color:'black'}} className="text-center">To achieve customer statifaction</h2>
+      <br />
+      <Cardvis/>
+    </>
+  );
+}
 
-    export default Homescreen
+export default Homescreen;
